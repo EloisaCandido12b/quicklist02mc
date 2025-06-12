51516bc1646c1147ff970e1db43751d8adb36dca
Eloisa Candido <eloisacandido335@gmail.com>
2025-06-12 02:35:34 -0300

    criacao check

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ function showItemsList() {
         sectionList.innerHTML += `
             <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}" ${item.checked ? 'checked' : ''}>
-                    <div class="custom-checkbox">
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                         <img src="./checked.svg" alt="checked">
                     </div>
-                    <label for="item-${index}">${item.name}</label>
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
                 <button onclick="removeItem('${item.name}')">
                     <img src="./trash-icon.svg" alt="trash-icon">
@@ -35,6 +35,12 @@ function showItemsList() {
             </div>
         `
     })
+}
+
+function checkItem(itemName) {
+    const item = items.find((item) => item.name === itemName)
+    item.checked = !item.checked
+    showItemsList()
 }
 
 function removeItem(itemName) {
